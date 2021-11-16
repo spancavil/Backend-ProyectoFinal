61709c44510875e81d1b468b6d6ce61060644544
spancavil <soancavil@gmail.com>
2021-11-16 15:19:09 -0300

    Last modify scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,10 +3,10 @@
 #navigate into our working directory where we have all our github files
 cd /var/www/myapp/server
 
-# #add npm and node to path
-# export NVM_DIR="$HOME/.nvm"	
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,15 +6,15 @@ DIR="/var/www/myapp/server"
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /var/www/myapp/server
 
-# #download node and npm (version using in dev)
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-# . ~/.nvm/nvm.sh
-# nvm install v16.10
+#download node and npm (version using in dev)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v16.10
 
 #create our working directory if it doesnt exist
-# if [ -d "$DIR" ]; then
-#   echo "${DIR} exists"
-# else
-#   echo "Creating ${DIR} directory"
-#   mkdir ${DIR}
-# fi
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
